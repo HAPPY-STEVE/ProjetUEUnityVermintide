5b35ac66c30aa794a353cf90a3fd5281c96e1a0a
Gabriel Hardy <60385281+HAPPY-STEVE@users.noreply.github.com>
2023-03-15 18:26:25 +0100

    Init
    - Création Scene template
    -Ajouts imports de package (dotTween, Cinemachine, set d'assets low poly donjon)
    - Script Changement de scène

---
+++ Assets/Scripts/Scene/LevelLoader.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelLoader
+{
+    public class LevelLoader: MonoBehaviour
+    {
+		public static void LoadScene(string s, bool additive = false, bool setActive = false)
+		{
+			if (s == null)
+			{
+				s = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+			}
+			UnityEngine.SceneManagement.SceneManager.LoadScene(
+				s, additive ? UnityEngine.SceneManagement.LoadSceneMode.Additive : 0);
+
+			if (setActive)
+			{
+				// to mark it active we have to wait a frame for it to load.
+				CallAfterDelay.Create(0, () => {
+					UnityEngine.SceneManagement.SceneManager.SetActiveScene(
+						UnityEngine.SceneManagement.SceneManager.GetSceneByName(s));
+				});
+			}
+		}
+
+		public static void UnloadScene(string s)
+		{
+			UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(s);
+		}
+	}
+
+	public class CallAfterDelay : MonoBehaviour
+	{
+		float delay;
+		System.Action action;
+
+		public static CallAfterDelay Create(float delay, System.Action action)
+		{
+			CallAfterDelay cad = new GameObject("CallAfterDelay").AddComponent<CallAfterDelay>();
+			cad.delay = delay;
+			cad.action = action;
+			return cad;
+		}
+
+		float age;
+
+		void Update()
+		{
+			if (age > delay)
+			{
+				action();
+				Destroy(gameObject);
+			}
+		}
+		void LateUpdate()
+		{
+			age += Time.deltaTime;
+		}
+	}
+}
+
